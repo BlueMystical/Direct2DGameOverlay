e4970fa0b88080e2f5b8fa2a05c6974312cb294e
BlueMystic <jhollman.chacon@gmail.com>
2023-08-16 16:28:56 -0300

    v1.0.1

---
+++ source/ModuleGUI.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace DirectXOverlay
+{
+	partial class ModuleGUI
+	{
+		/// <summary> 
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de componentes
+
+		/// <summary> 
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.chkEnabled = new System.Windows.Forms.CheckBox();
+			this.lblKeyBinds = new System.Windows.Forms.Label();
+			this.listLayers = new System.Windows.Forms.CheckedListBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.txtHotKey = new System.Windows.Forms.TextBox();
+			this.txtModuleName = new System.Windows.Forms.TextBox();
+			this.txtDescripcion = new System.Windows.Forms.TextBox();
+			this.cmdEditLayer = new System.Windows.Forms.Button();
+			this.cmdDeleteLayer = new System.Windows.Forms.Button();
+			this.cmdAddLayer = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// chkEnabled
+			// 
+			this.chkEnabled.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.chkEnabled.AutoSize = true;
+			this.chkEnabled.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.chkEnabled.Location = new System.Drawing.Point(437, 5);
+			this.chkEnabled.Name = "chkEnabled";
+			this.chkEnabled.Size = new System.Drawing.Size(68, 17);
+			this.chkEnabled.TabIndex = 2;
+			this.chkEnabled.Text = "Enabled:";
+			this.chkEnabled.UseVisualStyleBackColor = true;
+			this.chkEnabled.CheckedChanged += new System.EventHandler(this.chkEnabled_CheckedChanged);
+			// 
+			// lblKeyBinds
+			// 
+			this.lblKeyBinds.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblKeyBinds.AutoSize = true;
+			this.lblKeyBinds.Location = new System.Drawing.Point(264, 7);
+			this.lblKeyBinds.Name = "lblKeyBinds";
+			this.lblKeyBinds.Size = new System.Drawing.Size(45, 13);
+			this.lblKeyBinds.TabIndex = 3;
+			this.lblKeyBinds.Text = "HotKey:";
+			this.lblKeyBinds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// listLayers
+			// 
+			this.listLayers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listLayers.FormattingEnabled = true;
+			this.listLayers.Location = new System.Drawing.Point(9, 67);
+			this.listLayers.Name = "listLayers";
+			this.listLayers.Size = new System.Drawing.Size(470, 64);
+			this.listLayers.TabIndex = 4;
+			this.listLayers.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.listLayers_ItemCheck);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(6, 51);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Layers:";
+			// 
+			// txtHotKey
+			// 
+			this.txtHotKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtHotKey.Location = new System.Drawing.Point(312, 3);
+			this.txtHotKey.Name = "txtHotKey";
+			this.txtHotKey.ReadOnly = true;
+			this.txtHotKey.Size = new System.Drawing.Size(126, 20);
+			this.txtHotKey.TabIndex = 9;
+			this.txtHotKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.toolTip1.SetToolTip(this.txtHotKey, "- Doble Click to Edit.\r\n- ENTER to Save.\r\n- ESCAPE to Cancel.");
+			this.txtHotKey.DoubleClick += new System.EventHandler(this.txtHotKey_DoubleClick);
+			this.txtHotKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtHotKey_KeyDown);
+			this.txtHotKey.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHotKey_KeyPress);
+			// 
+			// txtModuleName
+			// 
+			this.txtModuleName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtModuleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtModuleName.ForeColor = System.Drawing.Color.Blue;
+			this.txtModuleName.Location = new System.Drawing.Point(9, 3);
+			this.txtModuleName.Name = "txtModuleName";
+			this.txtModuleName.ReadOnly = true;
+			this.txtModuleName.Size = new System.Drawing.Size(249, 20);
+			this.txtModuleName.TabIndex = 10;
+			this.txtModuleName.Text = "Module Name";
+			this.toolTip1.SetToolTip(this.txtModuleName, "- Doble Click to Edit.\r\n- ENTER to Save.\r\n- ESCAPE to Cancel.");
+			this.txtModuleName.DoubleClick += new System.EventHandler(this.txtModuleName_DoubleClick);
+			this.txtModuleName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtModuleName_KeyPress);
+			// 
+			// txtDescripcion
+			// 
+			this.txtDescripcion.Location = new System.Drawing.Point(9, 23);
+			this.txtDescripcion.Name = "txtDescripcion";
+			this.txtDescripcion.ReadOnly = true;
+			this.txtDescripcion.Size = new System.Drawing.Size(429, 20);
+			this.txtDescripcion.TabIndex = 11;
+			this.toolTip1.SetToolTip(this.txtDescripcion, "- Doble Click to Edit.\r\n- ENTER to Save.\r\n- ESCAPE to Cancel.");
+			this.txtDescripcion.DoubleClick += new System.EventHandler(this.txtDescripcion_DoubleClick);
+			this.txtDescripcion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDescripcion_KeyPress);
+			// 
+			// cmdEditLayer
+			// 
+			this.cmdEditLayer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdEditLayer.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.cmdEditLayer.FlatAppearance.BorderSize = 0;
+			this.cmdEditLayer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.cmdEditLayer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
+			this.cmdEditLayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.cmdEditLayer.Image = global::DirectXOverlay.Properties.Resources.editar_18;
+			this.cmdEditLayer.Location = new System.Drawing.Point(485, 111);
+			this.cmdEditLayer.Name = "cmdEditLayer";
+			this.cmdEditLayer.Size = new System.Drawing.Size(22, 22);
+			this.cmdEditLayer.TabIndex = 8;
+			this.toolTip1.SetToolTip(this.cmdEditLayer, "Edit Selected Layer");
+			this.cmdEditLayer.UseVisualStyleBackColor = true;
+			this.cmdEditLayer.Click += new System.EventHandler(this.cmdEditLayer_Click);
+			// 
+			// cmdDeleteLayer
+			// 
+			this.cmdDeleteLayer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdDeleteLayer.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.cmdDeleteLayer.FlatAppearance.BorderSize = 0;
+			this.cmdDeleteLayer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.cmdDeleteLayer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
+			this.cmdDeleteLayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.cmdDeleteLayer.Image = global::DirectXOverlay.Properties.Resources.cruz_18;
+			this.cmdDeleteLayer.Location = new System.Drawing.Point(485, 89);
+			this.cmdDeleteLayer.Name = "cmdDeleteLayer";
+			this.cmdDeleteLayer.Size = new System.Drawing.Size(22, 22);
+			this.cmdDeleteLayer.TabIndex = 7;
+			this.toolTip1.SetToolTip(this.cmdDeleteLayer, "Delete Selected Layer");
+			this.cmdDeleteLayer.UseVisualStyleBackColor = true;
+			this.cmdDeleteLayer.Click += new System.EventHandler(this.cmdDeleteLayer_Click);
+			// 
+			// cmdAddLayer
+			// 
+			this.cmdAddLayer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdAddLayer.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.cmdAddLayer.FlatAppearance.BorderSize = 0;
+			this.cmdAddLayer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.cmdAddLayer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
+			this.cmdAddLayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.cmdAddLayer.Image = global::DirectXOverlay.Properties.Resources.agregar_18;
+			this.cmdAddLayer.Location = new System.Drawing.Point(485, 67);
+			this.cmdAddLayer.Name = "cmdAddLayer";
+			this.cmdAddLayer.Size = new System.Drawing.Size(22, 22);
+			this.cmdAddLayer.TabIndex = 6;
+			this.toolTip1.SetToolTip(this.cmdAddLayer, "Add new Layer..");
+			this.cmdAddLayer.UseVisualStyleBackColor = true;
+			this.cmdAddLayer.Click += new System.EventHandler(this.cmdAddLayer_Click);
+			// 
+			// ModuleGUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.txtDescripcion);
+			this.Controls.Add(this.txtModuleName);
+			this.Controls.Add(this.txtHotKey);
+			this.Controls.Add(this.cmdEditLayer);
+			this.Controls.Add(this.cmdDeleteLayer);
+			this.Controls.Add(this.cmdAddLayer);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.listLayers);
+			this.Controls.Add(this.lblKeyBinds);
+			this.Controls.Add(this.chkEnabled);
+			this.Name = "ModuleGUI";
+			this.Size = new System.Drawing.Size(512, 134);
+			this.Load += new System.EventHandler(this.ModuleGUI_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.CheckBox chkEnabled;
+		private System.Windows.Forms.Label lblKeyBinds;
+		private System.Windows.Forms.CheckedListBox listLayers;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button cmdAddLayer;
+		private System.Windows.Forms.Button cmdDeleteLayer;
+		private System.Windows.Forms.Button cmdEditLayer;
+		private System.Windows.Forms.ToolTip toolTip1;
+		private System.Windows.Forms.TextBox txtHotKey;
+		private System.Windows.Forms.TextBox txtModuleName;
+		private System.Windows.Forms.TextBox txtDescripcion;
+	}
+}
---
+++ source/ModuleGUI.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+
+namespace DirectXOverlay
+{
+	public partial class ModuleGUI : UserControl
+	{
+        public OverlayModule Module { get; set; }
+
+		public delegate void ResourceAddedHandler(object sender, OnResourceAddedArgs e);
+		public event ResourceAddedHandler OnResourceAdded = delegate { }; //<- Evento con Manejador, para evitar los Null
+		
+		public event ResourceAddedHandler OnLayerAdded = delegate { }; //<- Evento con Manejador, para evitar los Null
+
+
+		/// <summary>List of pre-loaded Colors.</summary>
+		public Dictionary<string, Color> AvailableColors { get; set; }
+
+		/// <summary>List of pre-loaded Fonts</summary>
+		public Dictionary<string, Font> AvailableFonts { get; set; }
+
+		/// <summary>List of pre-loaded Textures</summary>
+		public Dictionary<string, Bitmap> AvailableTextures { get; set; }
+		public List<TextureEx> AvailableTexturesDescriptor { get; set; } = new List<TextureEx>();
+
+
+		private Keys _PressedKeys = Keys.None; //<- When assigning KeyBinds to a Module
+
+		public ModuleGUI()
+        {
+			InitializeComponent();
+        }
+        public ModuleGUI(OverlayModule pModule)
+		{
+			InitializeComponent();
+			this.Module = pModule;
+		}
+		private void ModuleGUI_Load(object sender, EventArgs e)
+		{
+			//Iconos de FontAwesome.Sharp:  https://github.com/awesome-inc/FontAwesome.Sharp#windows-forms
+			// Documentation:				https://awesome-inc.github.io/FontAwesome.Sharp/
+			// Lista de Iconos:				https://fontawesome.com/search?m=free
+
+			cmdAddLayer.Image =		IconChar.Plus.ToBitmap(Color.Gray, 18);
+			cmdDeleteLayer.Image =	IconChar.Trash.ToBitmap(Color.Gray, 18);
+			cmdEditLayer.Image =	IconChar.Pen.ToBitmap(Color.Gray, 18);
+
+			LoadModule(this.Module);
+		}
+
+		/// <summary>Loads and show the idicated module.</summary>
+		/// <param name="pModule">Module to load</param>
+		public void LoadModule(OverlayModule pModule)
+		{
+			try
+			{
+				if (pModule != null)
+				{
+					this.txtModuleName.Text = pModule.Name;
+					this.txtDescripcion.Text = pModule.Description;
+					this.chkEnabled.Checked = pModule.Enabled;
+
+					Keys _PressedKeys = (Keys)pModule.HotKeys;
+					this.txtHotKey.Text = new KeysConverter().ConvertToString(_PressedKeys);
+
+
+					if (pModule.Layers != null && pModule.Layers.Count > 0)
+					{
+						listLayers.Items.Clear();
+
+						foreach (var layer in pModule.Layers)
+						{
+							listLayers.Items.Add(layer, layer.Value.Visible);
+						}
+
+						listLayers.ValueMember = "Name";
+						listLayers.DisplayMember = "Description";
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + ex.StackTrace, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		public void ToggleEnabled(bool pEnabled)
+		{
+			try
+			{
+				Module.Enabled = pEnabled;
+				this.chkEnabled.Checked = pEnabled;
+
+				if (Module.Layers != null && Module.Layers.Count > 0)
+				{
+					listLayers.Items.Clear();
+
+					foreach (var layer in Module.Layers)
+					{
+						listLayers.Items.Add(layer, pEnabled);
+					}
+
+					listLayers.ValueMember = "Name";
+					listLayers.DisplayMember = "Description";
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + ex.StackTrace, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		/// <summary>Gets the data of the selected Layer.</summary>
+		public LayerEx GetSelectedLayer()
+		{
+			LayerEx _ret = null;
+			if (listLayers.SelectedItem != null)
+			{
+				KeyValuePair<string, LayerEx> _layer = ((KeyValuePair<string, LayerEx>)listLayers.SelectedItem);
+				_ret = _layer.Value;
+			}
+			else
+			{
+				_ret = ((KeyValuePair<string, LayerEx>)listLayers.Items[0]).Value;
+			}
+			return _ret;
+		}
+
+		private void chkEnabled_CheckedChanged(object sender, EventArgs e)
+		{
+			//Deshabilita todo el modulo
+			Module.Enabled = this.chkEnabled.Checked;
+		}
+
+		private void listLayers_ItemCheck(object sender, ItemCheckEventArgs e)
+		{
+			//Deshabilita la Capa seleccionada
+			if ((sender as CheckedListBox).SelectedItem != null)
+			{
+				KeyValuePair<string, LayerEx> _layer = ((KeyValuePair<string, LayerEx>)(sender as CheckedListBox).SelectedItem);
+
+				_layer.Value.Visible = (e.NewValue == CheckState.Checked ? true : false);
+			}
+		}
+
+		private void cmdAddLayer_Click(object sender, EventArgs e)
+		{
+			LayerEx layer = new LayerEx(string.Format("Layer_{0}", Module.Layers.Count + 1), "This is a new Layer.") { Kind = LayerType.Line };
+			LayerForm _Form = new LayerForm(layer)
+			{
+				AvailableColors = this.AvailableColors,
+				AvailableTextures = this.AvailableTextures,
+				AvailableFonts = this.AvailableFonts,
+				AvailableTexturesDescriptor = this.AvailableTexturesDescriptor,
+			};
+			if (_Form.ShowDialog() == DialogResult.OK)
+			{
+				layer = _Form.Layer;
+				this.Module.Layers.Add(layer.Name, layer);
+				//d3DOverlay.Layers.Add(string.Format("Module{0}_Layer{1}", modCounter, layerCounter), _layer.Value);
+
+				LoadModule(this.Module);
+
+				OnLayerAdded?.Invoke(this, new OnResourceAddedArgs() 
+				{
+					Layer = layer, 
+					AddedColors = _Form.AddedColors, 
+					AddedFonts = _Form.AddedFonts, 
+					AddedTextures = _Form.AddedTexturesEx 
+				});
+			}
+		}
+		private void cmdDeleteLayer_Click(object sender, EventArgs e)
+		{
+			// TODO:
+		}
+		private void cmdEditLayer_Click(object sender, EventArgs e)
+		{
+			var myLayer = GetSelectedLayer();
+
+			LayerForm _Form = new LayerForm(myLayer) 
+			{ 
+				AvailableColors = this.AvailableColors,
+				AvailableTextures = this.AvailableTextures,
+				AvailableFonts = this.AvailableFonts,
+				AvailableTexturesDescriptor = this.AvailableTexturesDescriptor,
+			};
+			if (_Form.ShowDialog() == DialogResult.OK)
+			{
+				myLayer = _Form.Layer;
+				LoadModule(this.Module);
+
+				if (_Form.IsDirty)
+				{
+					OnResourceAdded?.Invoke(this, new OnResourceAddedArgs() 
+					{ 
+						AddedColors = _Form.AddedColors, 
+						AddedFonts = _Form.AddedFonts, 
+						AddedTextures = _Form.AddedTexturesEx 
+					});
+				}
+			}
+		}
+
+		private void txtModuleName_DoubleClick(object sender, EventArgs e)
+		{
+			txtModuleName.ReadOnly = false;
+		}
+		private void txtModuleName_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//Se Presionó la Tecla ENTER
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				Module.Name = txtModuleName.Text;
+				txtModuleName.ReadOnly = true;
+			}
+			if (e.KeyChar == (char)Keys.Escape)
+			{
+				txtModuleName.Text = Module.Name;
+				txtModuleName.ReadOnly = true;
+			}
+		}
+
+		private void txtHotKey_DoubleClick(object sender, EventArgs e)
+		{
+			txtHotKey.ReadOnly = false;
+		}
+		private void txtHotKey_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			e.Handled = true;
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				Module.HotKeys = (int)_PressedKeys;
+				txtHotKey.ReadOnly = true;
+			}
+			if (e.KeyChar == (char) Keys.Escape)
+			{
+				_PressedKeys = (Keys)Module.HotKeys;
+				txtHotKey.Text = new KeysConverter().ConvertToString(_PressedKeys);
+				txtHotKey.ReadOnly = true;
+			}
+		}
+		private void txtHotKey_KeyDown(object sender, KeyEventArgs e)
+		{
+			if ((sender as TextBox).ReadOnly == false)
+			{
+				e.Handled = true;
+				if (e.KeyCode != Keys.Enter && e.KeyCode != Keys.Escape)
+				{
+					_PressedKeys = e.KeyData;
+					(sender as TextBox).Text = new KeysConverter().ConvertToString(_PressedKeys);
+
+					Console.WriteLine(string.Format("KeyDown:{0}, Value:{1}, KeyValue:{2}",
+						e.KeyData, (int)e.KeyData, Helper.GetKeyCode(e.KeyData)));
+				}
+			}						
+		}
+
+		private void txtDescripcion_DoubleClick(object sender, EventArgs e)
+		{
+			txtDescripcion.ReadOnly = false;
+		}
+		private void txtDescripcion_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				Module.Description = txtDescripcion.Text;
+				txtDescripcion.ReadOnly = true;
+			}
+			if (e.KeyChar == (char)Keys.Escape)
+			{
+				txtDescripcion.Text = Module.Description;
+				txtDescripcion.ReadOnly = true;
+			}
+		}
+	}
+}
